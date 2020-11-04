6c6cff02311fced45bedcced03b9501a73415c70
SavaSharif <savasharif@gmail.com>
2020-11-04 22:40:29 +0100

    Update deployment process

--- deploy-hadoop.sh
+++ deploy-hadoop.sh
@@ -15,6 +15,10 @@ chmod a+x install.sh
 # DO NOT: add the namenode to the workers list.
 while read worker; do
   echo "$worker"
+  # Remove all older folders which might still be on there.
+  ssh $worker rm -r ddps_1
+
+  # Clone the new repo in there.
   ssh $worker git clone https://github.com/Trottero/ddps_1 /home/$USER_TO_USE/ddps_1
   ssh $worker chmod a+x /home/$USER_TO_USE/ddps_1/install.sh
   ssh $worker /home/$USER_TO_USE/ddps_1/install.sh
