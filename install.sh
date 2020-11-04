daf9a5330a84729bea97e8c62fd65c2a07c6e7b0
SavaSharif <savasharif@gmail.com>
2020-11-05 00:58:49 +0100

    Update install

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@
 chmod a+x ./ddps_1/environmentvar.sh
 . ./ddps_1/environmentvar.sh
 
+mkdir /home/$USER_TO_USE/ddps_1/hadoopstorage
+mkdir /home/$USER_TO_USE/ddps_1/hadoopstorage/data
+mkdir /home/$USER_TO_USE/ddps_1/hadoopstorage/namenodedir
+
 # Download hadoop
 curl -o /home/$USER_TO_USE/ddps_1/hadoop.tar.gz https://apache.mirror.wearetriple.com/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
 
