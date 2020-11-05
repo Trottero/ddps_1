67ca2ada70beaba93d354b45aace24e6317a46c8
SavaSharif <savasharif@gmail.com>
2020-11-06 00:44:18 +0100

    Multishell deployment

--- deploy-hadoop.sh
+++ deploy-hadoop.sh
@@ -14,20 +14,26 @@
 chmod a+x ./ddps_1/install.sh
 . ./ddps_1/install.sh
 
+deploy_to_node(){
+  echo "$1"
+  # Remove all older folders which might still be on there.
+  echo "" | ssh $1 rm -r /home/$USER_TO_USE/ddps_1
+
+  # Clone the new repo in there.
+  echo "Cloning repo"
+  echo "" | ssh $1 git clone https://github.com/Trottero/ddps_1 /home/$USER_TO_USE/ddps_1
+  echo "Setting permissions and downloading hadoop"
+  echo "" | ssh $1 chmod a+x /home/$USER_TO_USE/ddps_1/install.sh
+  echo "" | ssh $1 /home/$USER_TO_USE/ddps_1/install.sh
+  echo "Finished configuring node $1"
+}
+
 # For every worker in the list, have them download the git repo.
 # And execute install.sh aswell.
 # DO NOT: add the namenode to the workers list.
 cat /home/$USER_TO_USE/ddps_1/hadoopconfig/workers | while read worker;
 do
-  echo "$worker"
-  # Remove all older folders which might still be on there.
-  echo "" | ssh $worker rm -r /home/$USER_TO_USE/ddps_1
-
-  # Clone the new repo in there.
-  echo "Cloning repo"
-  echo "" | ssh $worker git clone https://github.com/Trottero/ddps_1 /home/$USER_TO_USE/ddps_1
-  echo "Setting permissions and downloading hadoop"
-  echo "" | ssh $worker chmod a+x /home/$USER_TO_USE/ddps_1/install.sh
-  echo "" | ssh $worker /home/$USER_TO_USE/ddps_1/install.sh
-  echo "Finished configuring node $worker"
+  deploy_to_node "$worker" &
 done
+
+wait
