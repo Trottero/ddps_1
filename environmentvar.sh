ca8067ceb394d7ef8f3a508a4151e94aab8c52c3
SavaSharif <savasharif@gmail.com>
2020-11-06 00:21:14 +0100

    Remove sketch lib things

--- environmentvar.sh
+++ environmentvar.sh
@@ -13,8 +13,8 @@ export HADOOP_MAPRED_HOME=$HADOOP_HOME
 export HADOOP_COMMON_HOME=$HADOOP_HOME
 export HADOOP_HDFS_HOME=$HADOOP_HOME
 export YARN_HOME=$HADOOP_HOME
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
+# export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+# export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
 
 export HADOOP_HOME=/home/$USER_TO_USE/ddps_1/hadoop-3.3.0
 export HADOOP_PATH=$HADOOP_HOME
