3afda4bfe34ded9fa2049ba17c61e5ffaa0bed9a
SavaSharif <SavaSharif@gmail.com>
2020-11-06 23:34:08 +0100

    Fix issues regaring reloading

--- reload-configs-local.sh
+++ reload-configs-local.sh
@@ -1,4 +1,5 @@
 echo "Reloading configs"
 # Apply patch to hadoop config files
+. ~/ddps_1/environmentvar.sh
 
 cp ~/ddps_1/hadoopconfig/* $HADOOP_HOME/etc/hadoop
--- reload-configs.sh
+++ reload-configs.sh
@@ -1,5 +1,6 @@
 # reload-configs.sh
 # reloads configs on host aswell as all workers
+. ~/ddps_1/environmentvar.sh
 
 . ~/ddps_1/reload-configs-local.sh
 # also apply it to all of the worker nodes
