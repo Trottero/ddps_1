877f2b661ef5840394d550d3025f82019a786153
SavaSharif <SavaSharif@gmail.com>
2020-11-07 12:33:51 +0100

    Update configs

--- reload-configs-local.sh
+++ reload-configs-local.sh
@@ -1,5 +1,10 @@
 echo "Reloading configs"
 # Apply patch to hadoop config files
+
+# Update configs from the git
+cd ~/ddps_1
+git pull
+
 . ~/ddps_1/environmentvar.sh
 
 cp ~/ddps_1/hadoopconfig/* $HADOOP_HOME/etc/hadoop
