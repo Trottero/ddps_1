4b4e3214dc440f1601a71ea0512e015378ca38cf
Trottero <wrlxs@hotmail.com>
2020-11-09 14:29:33 +0100

    update

--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `grep_${job_id}` ( key STRING, field STRING ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;
-LOAD DATA INPATH `/data/grep-${job_id}/*` INTO TABLE `grep_${job_id}`;
+CREATE TABLE `grep_${hivevar:job_id}` ( key STRING, field STRING ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;
+LOAD DATA INPATH `/data/grep-${hivevar:job_id}/*` INTO TABLE `grep_${hivevar:job_id}`;
 CREATE TABLE `grep_${job_id}_select` ( key STRING, field STRING );
 SET mapreduce.input.fileinputformat.split.maxsize;
 SET mapreduce.input.fileinputformat.split.maxsize=134217728;
 SET mapreduce.input.fileinputformat.split.maxsize;
 SET dfs.blocksize;
-INSERT OVERWRITE TABLE `grep_${job_id}_select` SELECT * FROM `grep_${job_id}` WHERE field LIKE '%XYZ%';
+INSERT OVERWRITE TABLE `grep_${hivevar:job_id}_select` SELECT * FROM `grep_${hivevar:job_id}` WHERE field LIKE '%XYZ%';
