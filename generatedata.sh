975c4bd2d1a479ed94647f7a3d3081d41e19388c
Trottero <wrlxs@hotmail.com>
2020-11-10 14:24:11 +0100

    Make wait only wait for PIDS

--- generatedata.sh
+++ generatedata.sh
@@ -1,6 +1,7 @@
 
 jobid=0
 JOB_ORDER=()
+JOB_WAITS=()
 
 # JOBS=(1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 10 10 10 10 10 50 50 50 100 100 200 200 400 800 4800)
 JOBS=(1 2 10)
@@ -26,15 +27,19 @@ do
         "CREATE TABLE grep_${jobid} ( key STRING, field STRING ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE;\
         LOAD DATA INPATH '/data/grep-${jobid}/*' INTO TABLE grep_${jobid}; \
         CREATE TABLE grep_${jobid}_select ( key STRING, field STRING );" &
+    JOB_WAITS[$jobid]=$!
 
     JOB_ORDER[$jobid]=$jobid
     let "jobid+=1" 
 done
 echo "Waiting for tasks to complete!"
-wait
+wait ${JOB_WAITS[@]}
 
 # 40, 80 | 6, 10, 20
 JOB_ORDER=( $(echo "${JOB_ORDER[@]}" | sed -r 's/(.[^ ]* )/ \1 /g' | tr " " "\n" | shuf | tr -d " ") )
+
+JOB_WAITS=()
+jobid=0
 
 echo  ${JOB_ORDER[@]}
 for i in ${JOB_ORDER[@]}
@@ -45,9 +50,12 @@ do
         SET mapred.max.split.size=134217728;\
         SET mapred.min.split.size=134217728;\
         INSERT OVERWRITE TABLE grep_${i}_select SELECT * FROM grep_${i} WHERE field LIKE '%XYZ%';" &
+    JOB_WAITS[$jobid]=$!
+    let "jobid+=1" 
     sleep 14
 done
 echo "Done iterating through tasks, waiting 10 minutes for the rest of the tasks to finish now."
-wait
-sleep 600
+
+wait ${JOB_WAITS[@]}
+# sleep 600
 cd ~
